370b01253f76bd8400c400ca56698e93074f68cb
xpsiders <xpsiders@gmail.com>
2026-05-07 18:01:47 +0100

    Dockerise: Added Secret key

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ import sys
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
